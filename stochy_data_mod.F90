d9042e0bb7b1bd1e3b2719a4e2b60e66e7460dab
Dustin Swales <dustin.swales@noaa.gov>
2026-04-10 15:01:32 +0000

    Remove netcdf f77 interfaces

--- stochy_data_mod.F90
+++ stochy_data_mod.F90
@@ -91,7 +91,6 @@ module stochy_data_mod
    real(kind_dbl_prec),allocatable :: noise_e(:,:),noise_o(:,:)
    include 'function_indlsod'
    include 'function_indlsev'
-   include 'netcdf.inc'
    stochlun=99
    levs=nlevs
 
@@ -544,7 +543,6 @@ module stochy_data_mod
    real(kind=kind_dbl_prec) :: gamma_sum,pi
 
    real(kind_dbl_prec),allocatable :: noise_e(:,:),noise_o(:,:)
-   include 'netcdf.inc'
    stochlun=99
    levs=nlevs
 
@@ -834,7 +832,6 @@ subroutine read_pattern(rpattern,jcapin,lunptn,k,np,varid1,varid2,slice_of_3d,ir
    real(kind_dbl_prec) :: stdevin,varin
    integer nm,nn,iret,ierr,isize,k,ndimspec2
    integer, allocatable :: isave(:)
-   include 'netcdf.inc'
    iret=0
    ndimspec2=2*ndimspec
    allocate(pattern2d(ndimspec2))
